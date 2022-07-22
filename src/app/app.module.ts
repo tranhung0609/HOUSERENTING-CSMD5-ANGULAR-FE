c4822836b20a38eb56b8fa26a99c8edba0847771
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-07-22 15:46:57 +0700

    tạo font end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+import {environment} from "../environments/environment";
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
+import {AngularFireModule} from "@angular/fire/compat";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, "cloud"),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl : 'http://localhost:8003/login',
+  firebaseConfig : {
+    apiKey : "AIzaSyBrxQ0VM1pusl2Ey_J07gY8py0ch5_WhUs" ,
+    authDomain : "hydra-tvh.firebaseapp.com" ,
+    databaseURL : "https://hydra-tvh-default-rtdb.firebaseio.com" ,
+    projectId : "hydra-tvh" ,
+    storageBucket : "hydra-tvh.appspot.com" ,
+    messagingSenderId : "249986935010" ,
+    appId : "1:249986935010:web:ddf3c0806dd4d8b51a56bb" ,
+    measurementId : "G-HCDEHGS1ZS"
+  }
 };
 
 /*
