162e679a3fac31f11fb60d5dee372c66d538ba5e
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-07-25 23:15:36 +0700

    50%

---
+++ src/app/components/pages/users/add-home/add-home.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddHomeComponent } from './add-home.component';
+
+describe('AddHomeComponent', () => {
+  let component: AddHomeComponent;
+  let fixture: ComponentFixture<AddHomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddHomeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
